9b1663703c03ca87d869b027e7054df8a5e017b3
Frederman98 <55514109+Frederman98@users.noreply.github.com>
2019-11-24 12:34:19 -0600

    FreeRest 0.5
    ya hace intent desde la grafica de pastel

--- FreeRestApp/app/src/main/java/com/freerest/app/MainActivity.java
+++ FreeRestApp/app/src/main/java/com/freerest/app/MainActivity.java
@@ -3,22 +3,44 @@ package com.freerest.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.charts.RadarChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+
     PieChart pastel;
-    Button boton1;
+    RadarChart radar;
+
+    private float[] datosPastel = {25.3f, 10.6f, 66.76f, 44.32f, 46.01f};
+    private String[] etiquetasPastel = {"Unidad 1", "Uniadad 2" , "Unidad 3" , "Unidad 4", "Unidad 5"};
+
+    String[] labels = {"atencion recibida", "calidad de la comida", "calidad de bebidas", "relacion calidad-precio",
+            "horario"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +48,88 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pastel = (PieChart) findViewById(R.id.pastel1);
-        boton1 = (Button) findViewById(R.id.button);
-        boton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, grafico_barras.class);
-                startActivity(intent);
-            }
-        });
 
         crearGraficoPastel();
 
+        pastel.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                Log.d(TAG, "onValueSelected: Value select from chart.");
+                Log.d(TAG, "onValueSelected: " + e.toString());
+                Log.d(TAG, "onValueSelected: " + h.toString());
 
+                Intent intent = new Intent(MainActivity.this, radar.class);
+                intent.putExtra("DatosUN", h.toString());
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+        pastel.setHoleRadius(0);
+        pastel.setTransparentCircleAlpha(0);
+        pastel.setEntryLabelTextSize(0);
+
+//crenado graico de radar
+
+        radar = (RadarChart) findViewById(R.id.radar);
+
+        RadarDataSet dataSet = new RadarDataSet(dataValues(), "Satisfaccion");
+
+        dataSet.setColor(Color.BLUE);
+
+        RadarData data = new RadarData();
+        data.addDataSet(dataSet);
+
+        XAxis xAxis = radar.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+
+        radar.setData(data);
+        radar.invalidate();
+        radar.setScaleY(1.7f);
+        radar.setScaleX(1.7f);
+
+    }
+
+    private ArrayList<RadarEntry> dataValues(){
+        ArrayList<RadarEntry> dataVals = new ArrayList<>();
+        dataVals.add(new RadarEntry(5));
+        dataVals.add(new RadarEntry(6));
+        dataVals.add(new RadarEntry(1));
+        dataVals.add(new RadarEntry(9));
+        dataVals.add(new RadarEntry(10));
+        return dataVals;
     }
 
     private void crearGraficoPastel() {
 
-        Description description = new Description();
-        description.setText("Grafico de pastel");
-        pastel.setDescription(description);
-        //pastel.setUsePercentValues(true);
+        Log.d(TAG, "addDataSet started");
+        ArrayList<PieEntry> yEntrys = new ArrayList<>();
+        ArrayList<String> xEntrys = new ArrayList<>();
 
         ArrayList<PieEntry> pieEntries = new ArrayList<>();
 
-        //cambiar los valores por cant_u1 ... u4
+        for (int i=0; i<datosPastel.length; i++){
+            pieEntries.add(new PieEntry(datosPastel[i], etiquetasPastel[i]));
+        }
 
-        pieEntries.add(new PieEntry(1000, "unidad 1"));
-        pieEntries.add(new PieEntry(5300, "unidad 2"));
-        pieEntries.add(new PieEntry(4200, "unidad 3"));
-        pieEntries.add(new PieEntry(2580, "unidad 4"));
+        //add colors to dataset
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.GRAY);
+        colors.add(Color.BLUE);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.CYAN);
+        colors.add(Color.YELLOW);
+        colors.add(Color.MAGENTA);
 
-        PieDataSet pieDataSet = new PieDataSet(pieEntries, "descripcion");
-        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "");
+        pieDataSet.setColors(colors);
 
         PieData pieData = new PieData(pieDataSet);
 
